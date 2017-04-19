ed41c39d75a20bb9fc87fc5bfb6175e6b82141d2
cofauver <cofauver@gmail.com>
2017-04-19 15:41:56 -0700

    adds to the comments for clarity

--- solutions.js
+++ solutions.js
@@ -7,6 +7,8 @@ function merge(left, right) {
   var result = [];
 
   // while both arrays have elements in them, zip them together
+  // could also be written as:
+  // while (left.length > 0 && right.length > 0){
   while (left.length && right.length) {
     // if the left array first element is less than the right array first element, push to result
     if (left[0] <= right[0]) {
@@ -18,10 +20,15 @@ function merge(left, right) {
   }
 
   // if left is the only array with elements, push them all in
+  // could also be written as:
+  // while (left.length > 0){
   while (left.length) {
       result.push(left.shift());
   // if right is the only array with elements, push them all in
   }
+
+  // could also be written as:
+  // while (right.length > 0){
   while (right.length) {
     result.push(right.shift());
   }
